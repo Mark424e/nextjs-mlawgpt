3b55c8dcc8a8b552d131983058f2841ec334d268
Mark <markphillip1800@gmail.com>
2024-12-05 11:53:14 +0100

    header and footer

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import { Message } from "ai"
 import Bubble from "./components/Bubble"
 import LoadingBubble from "./components/LoadingBubble"
 import PromptSuggestionsRow from "./components/PromptSuggestionsRow"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 const Home = () => {
   const { append, isLoading, messages, input, handleInputChange, handleSubmit } = useChat()
@@ -22,34 +24,36 @@ const Home = () => {
   }
 
   return (
-    <main className="messages-container">
-      <Image src={MlawGPTLogo} width="100" alt="MlawGPT Logo"/>
-      <section className={noMessages ? "" : "populated"}>
-        {noMessages ? (
-          <>
-            <p className="px-20">
-              Rådfør dig med MlawGPT for omfattende juridisk indsigt og modtag de mest opdaterede svar.
-            </p>
-            <br/>
-            <PromptSuggestionsRow onPromptClick={handlePrompt}/>
-          </>
-        ) : (
-          <>
-            {messages.map((message, index) => <Bubble key={`message-${index}`} message={message}/>)}
-            {isLoading && <LoadingBubble/>}
-          </>
-        )}
-      </section>
-      <div className="w-full border-t border-t-secondary pt-5">
-        <form onSubmit={handleSubmit} className="h-16 w-full flex rounded-3xl overflow-hidden">
-          <input className="question-box" onChange={handleInputChange} value={input} placeholder="Skriv din besked..."/>
-          <input className="w-[15%] p-[10px] text-sm border-none text-white bg-primary transition hover:bg-primary/50 cursor-pointer" type="submit" value="Send"/>
-        </form>  
-      <div className="mt-5">
-        <p className="text-xs">Bemærk venligst, at denne AI-assistent giver svar baseret på den nyeste viden tilgængelig fra <a className="text-primary underline" href="https://mlaw.dk/" target="_blank">mlaw.dk</a>. For præcis assistance, undgå venligst at stille spørgsmål, der ikke er relateret.</p>
-      </div>
-      </div>
-    </main>
+    <>
+      <Header/>
+      <main className="flex items-center flex-col justify-between text-center">
+        <section className={noMessages ? "" : "populated"}>
+          {noMessages ? (
+            <>
+              <p className="px-20">
+                Rådfør dig med MlawGPT for omfattende juridisk indsigt og modtag de mest opdaterede svar.
+              </p>
+              <br/>
+              <PromptSuggestionsRow onPromptClick={handlePrompt}/>
+            </>
+          ) : (
+            <>
+              {messages.map((message, index) => <Bubble key={`message-${index}`} message={message}/>)}
+              {isLoading && <LoadingBubble/>}
+            </>
+          )}
+        </section>
+        <div className="w-full border-t border-t-secondary pt-5">
+          <form onSubmit={handleSubmit} className="h-16 w-full flex rounded-3xl overflow-hidden">
+            <input className="question-box" onChange={handleInputChange} value={input} placeholder="Skriv din besked..."/>
+            <input className="w-[15%] p-[10px] text-sm border-none text-white bg-primary transition hover:bg-primary/50 cursor-pointer" type="submit" value="Send"/>
+          </form>  
+        <div className="mt-5">
+          <p className="text-xs">Bemærk venligst, at denne AI-assistent giver svar baseret på den nyeste viden tilgængelig fra <a className="text-primary underline" href="https://mlaw.dk/" target="_blank">mlaw.dk</a>. For præcis assistance, undgå venligst at stille spørgsmål, der ikke er relateret.</p>
+        </div>
+        </div>
+      </main>
+    </>
   )
 }
 
